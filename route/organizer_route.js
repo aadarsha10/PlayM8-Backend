d30330425588b92529aebd5924e00106e6091528
Aadarsha Man Shrestha <a.shrestha998@gmail.com>
2021-07-10 19:33:54 +0545

    worked on organizer login

--- route/organizer_route.js
+++ route/organizer_route.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router()
 const organizer = require('../models/organizer_detail_model')
 const { check, validationResult } = require('express-validator')
-
+const bcryptjs = require('bcryptjs')   //for encryption, done after validation(npm i bcrypt)
+const jwt = require('jsonwebtoken')   //for token npm i jsonwebtoken --save
 
 router.post('/createOrganizer', [
 
@@ -19,7 +20,6 @@ router.post('/createOrganizer', [
 
 
 ], function (req, res) {
-
     //    if(req.body == undefined)
     // {
     //     return res.status(400).json({message: "Invalid file format"})
@@ -33,7 +33,6 @@ router.post('/createOrganizer', [
         const Username = req.body.Username
         const Email = req.body.Email
         const Password = req.body.Password
-
         const org_details = new organizer({
             Fullname: Fullname,
             Address: Address,
@@ -42,9 +41,7 @@ router.post('/createOrganizer', [
             Email: Email,
             Password: Password
         })
-
         console.log("details:", org_details)
-
         org_details.save().then(function (result) {
             res.json({
                 message: "Registered Succesfully!"
@@ -59,6 +56,40 @@ router.post('/createOrganizer', [
         res.send({ error: error })
         //error validation for every details of organizer
     }
+})
+
+//login system
+router.post('/login/organizer', function (req, res) {
+    const userName = req.body.username
+    const password = req.body.password   //user provided password
+    //we need to find if user exists
+    user.findOne({ userName: userName })    //first variable userName is from user_model while the second is created here
+        .then(function (userData) {
+            if (userData === null) {
+                return res.status(403).json({ success: false, userData: null })
+            }
+            //username is correct
+            bcryptjs.compare(password, userData.password, function (err, result) { //first password is variable and another is db password
+                if (result === false) {
+                    return res.status(403).json({ success: false, message: "Invalid username or password!" })
+                }
+                // res.send("Correct")
+                const token = jwt.sign({ userId: userData._id }, 'secretkey')  //providing token
+                res.status(200).json({
+                    success: true,
+                    token: token,
+                    Username: userData.userName,
+                    Email: userData.email,
+                    Lastname: userData.lastName,
+                    Firstname: userData.firstName
+                })
+
+
+            })
+        })
+        .catch(function (err) {
+            res.status(500).json({ message: err })
+        })
 })
 
 
