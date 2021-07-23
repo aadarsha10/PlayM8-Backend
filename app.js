bb0f70912388ff5e45d9e6630bc744df05602d15
santooshrestha <shrestha.santoo@gmail.com>
2021-07-23 17:21:08 +0545

    admin request

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-const express = require('express');
+const express = require("express");
 
-const cors = require('cors')
-const route_organizer_details = require('./route/organizer_route')
-const route_admin = require('./route/admin_route')
-const db = require('./database/db');
-const bodyParser = require('body-parser');
-const playerSignup_route = require('./route/playerSignup_route')
-
+const cors = require("cors");
+const route_organizer_details = require("./route/organizer_route");
+const route_admin = require("./route/admin_route");
+const db = require("./database/db");
+const bodyParser = require("body-parser");
+const playerSignup_route = require("./route/playerSignup_route");
+const request_route = require("./route/admin_request_route");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-app.use(route_organizer_details)
-app.use(route_admin)
-app.use(playerSignup_route)
-
+app.use(route_organizer_details);
+app.use(route_admin);
+app.use(playerSignup_route);
+app.use(request_route);
 
 // app.listen(90);
-app.listen(process.env.PORT || 90)
+app.listen(process.env.PORT || 90);
